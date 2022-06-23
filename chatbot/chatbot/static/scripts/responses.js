96b38f733e3ac6407c3dbb60bd905e46497840c4
BalajiNamala <108024594+BalajiNamala@users.noreply.github.com>
2022-06-23 10:55:29 +0530

    Update responses.js

--- chatbot/chatbot/static/scripts/responses.js
+++ chatbot/chatbot/static/scripts/responses.js
@@ -1,11 +1,17 @@
 function getBotResponse(input) {
     //rock paper scissors
     if (input == "departments") {
-        return "Computer Science and Enginering \n Information Technology \n Electrical Engineering \n Civil Engineering";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
+        return "1.Computer Science and Enginering \n 2.Information Technology \n 3.Electrical Engineering \n 4.Civil Engineering";
+    } else if (input == "transport") {
+        return "There are no of transport ways which can be used to get to the college. Our own college transport will pick up students from across the city";
+    } else if (input == "CSE") {
+        return "Abc xxxxxxxxx";
+    } else if (input=="IT"){
+        return "Def xxxxxxxxxx";
+    } else if (input=="EEE"){
+        return "NFN xxxxxxxxx";
+    } else if (input=="detailed departments"){
+        return "Which department - CSE, IT, EEE";
     }
 
     // Simple responses
